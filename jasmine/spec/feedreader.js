f08687f84b02478e1caaccbd1e1da840483fcb35
difo <xiaoxinquwenwen@163.com>
2015-06-16 16:48:06 +0800

    fixed last test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -24,25 +24,25 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('has url', function() {
+        it('has url', function() {
             for(var i=0; i<allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
                 expect(allFeeds[i].url).not.toBe(0);
             }
-         });
+        });
 
 
         /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.Testing for a type of string and length>0.
          */
-         it('has name', function() {
+        it('has name', function() {
             for(var i=0; i<allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
                 expect(typeof allFeeds[i].name === 'string').toBe(true);
                 expect(allFeeds[i].name.length > 0).toBe(true);
             }
-         });
+        });
     });
 
 
@@ -54,21 +54,21 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-         it('the menu is hidden', function() {
+        it('the menu is hidden', function() {
             expect($('body').attr('class','menu-hidden')).toBeDefined();
-         });
+        });
 
          /* This is a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-          it('the menu changes when icon is clicked',function() {
+		it('the menu changes when icon is clicked',function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
+        });
     });
 
     /* This is a new test suite named "Initial Entries" */
@@ -81,18 +81,18 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-         //use beforeEach and done function because loadFeed() is asynchronous .
-         beforeEach(function(done) {
+        //use beforeEach and done function because loadFeed() is asynchronous .
+        beforeEach(function(done) {
             loadFeed(1,done);        
          });
 
-         it('have entry element when the loadFeed function is done', function() {
+        it('have entry element when the loadFeed function is done', function() {
             expect($('.feed').find('.entry')).toBeDefined();
-         });
+        });
 
 
         //use afterEach to set back to the iniatial statement, avoiding errors.
-         afterEach(function (done) {
+        afterEach(function (done) {
             loadFeed(0, done);
         });
     });
@@ -105,25 +105,27 @@ $(function() {
          * Remember, loadFeed() is asynchronous.
          */
 
-         $('.feed').empty();
+        $('.feed').empty();
          
-         // set currentFeed outside, for further comparing.
-         var currentFeed;
+        // set currentFeed outside, for further comparing.
+        var currentFeed;
 
-         beforeEach(function(done) {
-            //get the current content of feed.
-            currentFeed = $('.feed').html();
-            loadFeed(1,done);        
-         });
+        beforeEach(function (done) {
+            loadFeed(0, function() {
+                currentFeed = $('.feed').html();
+                loadFeed(1,function() {
+                    done();
+                }); 
+            });       
+        });
 
-         it('content actually changes when a new feed loads', function() {
-            //get the refreshed content of feed,then comparing it with the initial one.
-            var refreshedFeed = $('.feed').html();
-            expect(currentFeed).not.toBe(refreshedFeed);
-         });
+        it('content actually changes when a new feed loads', function (done) {
+            expect($('.feed').html()).not.toBe(currentFeed);
+            done();
+        });
 
-         //use afterEach to set back to the iniatial statement, avoiding errors.
-         afterEach(function (done) {
+        //use afterEach to set back to the iniatial statement, avoiding errors.
+        afterEach(function (done) {
             loadFeed(0, done);
         });
     });
